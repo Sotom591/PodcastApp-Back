454bf3b5e8223264a0a042818ab3f7e48ecf7997
jennyjean <jennyjean8675309@yahoo.com>
2019-02-15 15:57:32 -0500

    Added faker gem to Gemfile, and seed data for users

--- Gemfile
+++ Gemfile
@@ -18,7 +18,7 @@ gem 'bcrypt', '~> 3.1.7'
 
 gem "jwt", "~> 2.1"
 
-
+gem 'faker'
 # Use ActiveStorage variant
 gem 'mini_magick', '~> 4.8'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,55 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+#Users (have a name, username, password, avatar, and emal)
+User.create(
+  name: 'Sean',
+  username: 'sean2019',
+  password: 'password',
+  avatar: Faker::Avatar.image,
+  email: 'sean@gmail.com'
+)
+
+User.create(
+  name: 'Marissa',
+  username: 'Sotom591',
+  password: 'password',
+  avatar: Faker::Avatar.image,
+  email: 'marissa@gmail.com'
+)
+
+User.create(
+  name: 'Mike',
+  username: 'mkim4247',
+  password: 'password',
+  avatar: Faker::Avatar.image,
+  email: 'mike@gmail.com'
+)
+
+User.create(
+  name: 'Beth',
+  username: 'bethwilson07',
+  password: 'password',
+  avatar: Faker::Avatar.image,
+  email: 'beth@gmail.com'
+)
+
+User.create(
+  name: 'Jenny',
+  username: 'jennyjean8675309',
+  password: 'password',
+  avatar: Faker::Avatar.image,
+  email: 'jenny@gmail.com'
+)
+
+50.times do
+  User.create(
+    name: Faker::FunnyName.name,
+    username: Faker::Internet.username,
+    password: Faker::Internet.password(8, 16),
+    avatar: Faker::Avatar.image,
+    email: Faker::Internet.email
+  )

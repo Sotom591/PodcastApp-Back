f499d729b254df9273d0ae21780f1649517a06ad
jennyjean <jennyjean8675309@yahoo.com>
2019-03-04 09:23:26 -0500

    Fixed an attribute on UserSerializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :firstName, :lastName :username, :avatar, :email, :friends, :friendships, :inverse_friendships, :pending_friends, :friend_requests
+  attributes :id, :firstName, :lastName, :username, :avatar, :email, :friends, :friendships, :inverse_friendships, :pending_friendships, :friend_requests
   has_many :podcasts
   has_many :genres
   has_many :episode_posts
